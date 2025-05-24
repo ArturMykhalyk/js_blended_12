a9cc6be38abe94b65791c9b9727de43030590191
SotnikovSS <sotnikovss90@gmail.com>
2025-05-24 18:34:06 +0200

    UPDT

--- src/js/render-function.js
+++ src/js/render-function.js
@@ -14,3 +14,19 @@ export function createCategories(categories) {
 
   refs.categories.innerHTML = markup;
 }
+
+export function cratedProducts(products){
+const markup = products.map(({ id, title, description, 
+brand, thumbnail, category, price }) =>
+`<li class="products__item" data-id="${id}">
+    <img class="products__image" src="${thumbnail}" alt="${description}"/>
+    <p class="products__title">${title}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand:${brand}</span></p>
+    <p class="products__category">Category:${category} </p>
+    <p class="products__price">Price:${price} $</p>
+ </li>`
+).join('');
+refs.products.insertAdjacentHTML('beforeend', markup);
+};
+
+
